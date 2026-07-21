92b4f57045881d25a44668277991a85c56dd875a
nowayrm <your.email@example.com>
2026-07-21 13:47:59 +0300

    some updates

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,5 @@
 import { headers } from 'next/headers'
 import type { Metadata, Viewport } from 'next'
-import Script from 'next/script'
 import './globals.css'
 import './west-auto.css'
 import UtmCapture from './components/UtmCapture'
@@ -17,8 +16,6 @@ import {
   SITE_URL,
   absoluteUrl,
 } from './seo'
-
-const GTM_ID = 'GTM-KNX6TD98'
 
 export const metadata: Metadata = {
   metadataBase: new URL(SITE_URL),
@@ -113,13 +110,6 @@ export default async function RootLayout({ children }: { children: React.ReactNo
   return (
     <html lang={htmlLang}>
       <head>
-        <Script id="google-tag-manager" strategy="beforeInteractive">
-          {`(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','${GTM_ID}');`}
-        </Script>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link
@@ -128,15 +118,6 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
         />
       </head>
       <body>
-        <noscript>
-          <iframe
-            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
-            height="0"
-            width="0"
-            style={{ display: 'none', visibility: 'hidden' }}
-            title="Google Tag Manager"
-          />
-        </noscript>
         <a href="#main-content" className="skip-link">
           Skip to content
         </a>
